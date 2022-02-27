fdb92969e06b6fe6a2ac8ad17a2971ab223d0bed
Дарья Дубровская <>
2022-02-27 23:39:14 +0300

    Task 22: Added file

---
+++ HomeWork22/HomeWork22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork22
+//
+//  Created by Дарья Дубровская on 27.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
